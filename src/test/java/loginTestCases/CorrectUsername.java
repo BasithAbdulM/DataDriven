c568de130ba232aaef9f872d7cf94820efb2b78b
BasithAbdulM <abdul.basith332@gmail.com>
2022-09-29 16:29:11 +0530

    Initial commit

---
+++ src/test/java/loginTestCases/CorrectUsername.java
@@ -0,0 +1,32 @@
+package loginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CorrectUsername {
+    WebDriver driver=new ChromeDriver();
+
+    @Test
+    @Parameters({"username","password"})
+    public void loginwithCU(String Uname,String Pword){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement username= driver.findElement(By.xpath("//input[@name='username']"));
+        username.sendKeys(Uname);
+
+        WebElement password=driver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys(Pword);
+
+        WebElement login= driver.findElement(By.xpath("//button[@type='submit']"));
+        login.click();
+        driver.quit();
+    }
+}
